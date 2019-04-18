4da0fd4ce654e2264a1fa935b3765e74e4b4c2ec
hard88 <wrh0813@gmail.com>
2019-04-18 16:12:58 +0800

    proverb

---
+++ proverb/proverb.go
@@ -0,0 +1,24 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package proverb should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package proverb
+
+import "fmt"
+
+// Proverb should have a comment documenting it.
+func Proverb(rhyme []string) []string {
+
+	var text []string
+
+	for i := range rhyme {
+		if i == len(rhyme) - 1 {
+			text = append(text, fmt.Sprintf("And all for the want of a %s.", rhyme[0]))
+		} else  {
+			text = append(text, fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1]))
+		}
+	}
+
+	return text
+}
